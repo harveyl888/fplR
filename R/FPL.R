7d9d09d7ab4d70bdd0f8f71d1f673a1401390378
Harvey Lieberman <harvey.lieberman@gmail.com>
2016-08-14 22:43:11 -0400

    get league table and entry details

--- R/FPL.R
+++ R/FPL.R
@@ -1,10 +1,36 @@
-#'FPLScrape
+#'getFPLData
 #'
-#'Scrape team, player and game data
+#'Get team, player and game data
 #'
 #' @import jsonlite
-FPLScrape <- function() {
+getFPLData <- function() {
   url <- 'https://fantasy.premierleague.com/drf/bootstrap-static'
   data <- jsonlite::fromJSON(url)
   return(data)
 }
+
+#'getLeagueTable
+#'
+#'Get a league table
+#'
+#' @import jsonlite
+getLeagueTable <- function(leagueID = NULL, wk = 1) {
+  if (is.null(leagueID)) return()
+  url <- paste0('https://fantasy.premierleague.com/drf/leagues-classic-standings/', leagueID, '?phase=', wk)
+  data <- jsonlite::fromJSON(url)
+  return(data)
+}
+
+#'getEntry
+#'
+#'Get an entry info
+#'
+#' @import jsonlite
+getEntry <- function(entry = NULL) {
+  if (is.null(entry)) return()
+  url <- paste0('https://fantasy.premierleague.com/drf/entry/', entry)
+  data <- jsonlite::fromJSON(url)
+  return(data)
+}
+
+
