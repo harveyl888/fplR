d6ba2274af2a918d114b90dcdb7dff33a57daf1d
Harvey Lieberman <harvey.lieberman@gmail.com>
2018-05-07 13:52:56 -0400

    bugfix - need to remove bench players

--- R/fpl_db_fns.R
+++ R/fpl_db_fns.R
@@ -1,4 +1,4 @@
-#' Get a league table
+#' Get weekly scores
 #'
 #' Retrieve weekly scores for a fantasy football league
 #'
@@ -10,13 +10,14 @@
 #' @import dplyr
 #' @import tidyr
 #' @export
-fpl_league <- function(l, weeks = c()) {
+fpl_league_weekly <- function(l, weeks = c()) {
   if (length(weeks) == 0) weeks <- seq(max(l[['league_weeks']]$week))
   df <- l[['league_weeks']] %>%
     filter(week %in% weeks) %>%
     left_join(l[['stats']] %>% select(id, week, total_points), by = c('element' = 'id', 'week')) %>%
     mutate(score = total_points * multiplier) %>%
     group_by(entry, week) %>%
+    slice(1:11) %>%
     summarise(points = sum(score)) %>%
     left_join(l[['league']], by = c('entry')) %>%
     unite(team, player_name, entry_name) %>%
