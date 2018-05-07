d2379ae17dca7cd3c8ade6a23b756391fea9ed47
Harvey Lieberman <harvey.lieberman@gmail.com>
2018-05-07 12:53:23 -0400

    return weekly scores

---
+++ R/fpl_db_fns.R
@@ -0,0 +1,28 @@
+#' Get a league table
+#'
+#' Retrieve weekly scores for a fantasy football league
+#'
+#' @param l List of obtained from read_database
+#' @param weeks Vector of weeks.  If empty then include all weeks
+#'
+#' @return dataframe containing table
+#'
+#' @import dplyr
+#' @import tidyr
+#' @export
+fpl_league <- function(l, weeks = c()) {
+  if (length(weeks) == 0) weeks <- seq(max(l[['league_weeks']]$week))
+  df <- l[['league_weeks']] %>%
+    filter(week %in% weeks) %>%
+    left_join(l[['stats']] %>% select(id, week, total_points), by = c('element' = 'id', 'week')) %>%
+    mutate(score = total_points * multiplier) %>%
+    group_by(entry, week) %>%
+    summarise(points = sum(score)) %>%
+    left_join(l[['league']], by = c('entry')) %>%
+    unite(team, player_name, entry_name) %>%
+    ungroup() %>%
+    select(team, week, points) %>%
+    spread(week, points) %>%
+    separate(team, into = c('manager', 'team'), sep = '_')
+  df
+}
