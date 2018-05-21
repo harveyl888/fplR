b9ccec6a09deec36687ba61485182dcc8fcc99c9
Harvey Lieberman <harvey.lieberman@gmail.com>
2018-05-21 13:21:06 -0400

    account for chips in weekly score

--- R/fpl_db_fns.R
+++ R/fpl_db_fns.R
@@ -1,3 +1,14 @@
+.week_score <- function(d, inc_transfers, entry_row) {
+  if (entry_row$chip == '3xc') {  ## account for triple captain
+    d[d$multiplier == 2, ]$multiplier <- 3
+  }
+  if (!entry_row$chip == 'bboost') {  ## account for bench boost
+    d <- d[1:11, ]
+  }
+  sum(d$total_points * d$multiplier)
+}
+
+
 #' Get weekly scores
 #'
 #' Retrieve weekly scores for a fantasy football league
@@ -12,13 +23,15 @@
 #' @export
 fpl_league_weekly <- function(l, weeks = c()) {
   if (length(weeks) == 0) weeks <- seq(max(l[['league_weeks']]$week))
+  entry_use <- l[['entry_weeks']] %>%
+    rename(use_entry = entry, use_week = week)
+
   df <- l[['league_weeks']] %>%
     filter(week %in% weeks) %>%
     left_join(l[['stats']] %>% select(id, week, total_points), by = c('element' = 'id', 'week')) %>%
-    mutate(score = total_points * multiplier) %>%
     group_by(entry, week) %>%
-    slice(1:11) %>%
-    summarise(points = sum(score)) %>%
+    do(points = .week_score(., inc_transfers = FALSE, entry_row = entry_use %>% filter(entry == use_entry & week == use_week))) %>%
+    mutate(points = unlist(points)) %>%
     left_join(l[['league']], by = c('entry')) %>%
     unite(team, player_name, entry_name) %>%
     ungroup() %>%
@@ -27,3 +40,4 @@ fpl_league_weekly <- function(l, weeks = c()) {
     separate(team, into = c('manager', 'team'), sep = '_')
   df
 }
+
